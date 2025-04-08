4a448a9306923c28f35b911348358a7ca0b0ff04
JonathanZopf <jonathanzopf@gmail.com>
2025-04-08 19:28:02 +0200

    Minor improvements in main function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,14 @@ package org.hszg
 import org.hszg.Solvers.RecursiveSolver
 
 fun main() {
-    val queensToPlace = 8
+    val queensToPlace = 12
     val solver = RecursiveSolver()
-    val startingTime = System.currentTimeMillis()
+    val startingTimeSolve = System.currentTimeMillis()
     val allSolutions = solver.solve(queensToPlace)
-    val endingTime = System.currentTimeMillis()
+    val endingTimeSolve = System.currentTimeMillis()
+    val startingTimeFilter = System.currentTimeMillis()
     val uniqueSolutions = filterOnlyUniqueSolutions(allSolutions)
+    val endingTimeFilter = System.currentTimeMillis()
 
     for ((i, board) in uniqueSolutions.withIndex()) {
         println("Unique Solution ${i + 1}:")
@@ -16,7 +18,9 @@ fun main() {
         println()
     }
 
-    println("Found ${uniqueSolutions.size} unique solutions out of ${allSolutions.size} total solutions in ${endingTime - startingTime} ms.")
+    println("Found ${uniqueSolutions.size} unique solutions out of ${allSolutions.size} total solutions.")
+    println("Time taken to solve: ${endingTimeSolve - startingTimeSolve} ms")
+    println("Time taken to filter unique solutions: ${endingTimeFilter - startingTimeFilter} ms")
 }
 
 private fun filterOnlyUniqueSolutions(allSolutions: List<Chessboard>): List<Chessboard> {
